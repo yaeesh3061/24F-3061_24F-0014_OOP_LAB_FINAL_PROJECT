040b8d29dd00228e5593a2f906a4e4442429f2d1
yaeesh3061 <f243061@cfd.nu.edu.pk>
2025-05-15 03:41:28 +0500

    Module-1.cpp

--- population.cpp
+++ population.cpp
@@ -1,66 +1,82 @@
-// Population.cpp
-#include "Stronghold.h"  
+#include "Stronghold.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-Population::Population(int initial) : total(initial), employed(0), healthy(initial) {}
+Population::Population() : count(100) {}
 
-void Population::grow() {
-    total += 5;
-    cout << "\nPopulation grew.";
-    cout << "\nTotal Population: " << total << endl;
-}
-
-void Population::shrink(int amount) {
-    if (total - amount > 0){
-        total -= amount;
-        cout << "\nPopulation reduced by " << amount;
-        cout << "\nTotal pouplation: " << total;
+void Population::modify(int amt) {
+    count += amt;
+    if (count < 0) {
+        count = 0;
+        cout << "Warning: Population has been completely wiped out!\n";
     }
     else {
-        total = 0;
-        cout << "\nPopulation cannot be reduced.";
+        cout << "Population changed by " << amt << ". New population: " << count << ".\n";
     }
-        
-
 }
 
-void Population::fallIll(int count) {
-    if (healthy - count > 0) {
-        healthy -= count;
-        cout << "\n" << count << " people fell ill.";
+void Population::naturalChange() {
+    if (isDefeated()) {
+        cout << "Kingdom is collapsed. No population changes occur.\n";
+        return;
+    }
+    int randFactor = rand() % 100;
+    int change = 0;
+    if (randFactor < 30) {
+        change = -(rand() % 5 + 1); // Decrease by 1-5
+        cout << "Some citizens have passed away due to natural causes or migrated.\n";
+    }
+    else if (randFactor < 70) {
+        change = rand() % 3 + 1; // Increase by 1-3
+        cout << "New births and settlers have increased your population.\n";
+    }
+    if (change != 0) {
+        modify(change);
+    }
+}
+
+void Population::display() const {
+    cout << "Population Status:\n";
+    cout << "  Total citizens: " << count << endl;
+    cout << "  Population state: ";
+    if (count > 500) {
+        cout << "Thriving metropolis";
+    }
+    else if (count > 300) {
+        cout << "Growing city";
+    }
+    else if (count > 200) {
+        cout << "Modest town";
+    }
+    else if (count > 100) {
+        cout << "Small settlement";
+    }
+    else if (count > 50) {
+        cout << "Struggling village";
+    }
+    else if (count > 0) {
+        cout << "Nearly abandoned";
     }
     else {
-        healthy = 0;
-        cout << "\nNo people fell ill.";
+        cout << "Ghost town";
     }
-}
-
-void Population::updateEmployment(int workers) {
-    employed = (workers < total) ? workers : total;
-    cout << "\n" << employed << " people are employed out of " << total;
+    cout << endl;
 }
 
 int Population::getTotal() const {
-    return total;
+    return count;
 }
 
-int Population::getUnemployed() const {
-    return (total - employed > 0) ? total - employed : 0;
+void Population::setTotal(int newCount) {
+    count = newCount;
+    if (count < 0) {
+        count = 0;
+        cout << "Warning: Population cannot be negative. Set to 0.\n";
+    }
 }
 
-void Population::printStatus() const {
-    cout << "\nPopulation: " << total << " | Healthy: " << healthy
-        << " | Employed: " << employed << endl;
-}
-
-void Population::saveToFile(ofstream& out) const {
-    out << total << "\n";
-    out << employed << "\n";
-    out << healthy << "\n";
-}
-
-void Population::loadFromFile(ifstream& in) {
-    string line;
-    getline(in, line); total = stoi(line);
-    getline(in, line); employed = stoi(line);
-    getline(in, line); healthy = stoi(line);
+bool Population::isDefeated() const {
+    return count <= 0;
 }
