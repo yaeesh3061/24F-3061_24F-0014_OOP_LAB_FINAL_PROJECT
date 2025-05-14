c59e6575ae7d20f8ecf7633ed6e52c949626c275
yaeesh3061 <f243061@cfd.nu.edu.pk>
2025-05-15 03:19:52 +0500

    Module-1.cpp

--- Economy.cpp
+++ Economy.cpp
@@ -1,53 +1,92 @@
-// Economy.cpp
 #include "Stronghold.h"
+#include <iostream>
+using namespace std;
 
-Economy::Economy(float tax) : taxRate(tax), inflation(0.0f), publicSpending(0.0f) {}
+Economy::Economy() : taxRate(10) {}
 
-void Economy::adjustTax(float delta) {
-    taxRate += delta;
+void Economy::manage(ResourceManager& rm, Peasant& peasant, Merchant& merchant, Noble& noble, int playerId) {
+    int choice;
+    cout << "\n===== Economy Management =====\n";
+    cout << "1. Set Tax Rate\n";
+    cout << "2. Collect Taxes\n";
+    cout << "3. Invest in Economy\n";
+    cout << "4. Back to Main Menu\n";
+    cout << "Enter choice: ";
+    cin >> choice;
+
+    switch (choice) {
+    case 1: {
+        cout << "Current tax rate: " << taxRate << "%\n";
+        cout << "Enter new tax rate (1-50): ";
+        int newRate;
+        cin >> newRate;
+        if (newRate >= 1 && newRate <= 50) {
+            int oldRate = taxRate;
+            taxRate = newRate;
+            int change = oldRate - newRate; // Positive if taxes went down
+            peasant.updateSatisfaction(change * 2);
+            merchant.updateSatisfaction(change);
+            noble.updateSatisfaction(change / 2);
+            cout << "Tax rate changed from " << oldRate << "% to " << taxRate << "%.\n";
+            peasant.displayStatus();
+            merchant.displayStatus();
+            noble.displayStatus();
+        }
+        else {
+            cout << "Invalid tax rate. Must be between 1 and 50.\n";
+        }
+        break;
+    }
+    case 2: {
+        int peasantTax = 1 * taxRate;
+        int merchantTax = 3 * taxRate;
+        int nobleTax = 5 * taxRate;
+        int totalTax = peasantTax + merchantTax + nobleTax;
+        rm.gather("Gold", totalTax, playerId);
+        cout << "Collected taxes:\n";
+        cout << "  From peasants: " << peasantTax << " gold\n";
+        cout << "  From merchants: " << merchantTax << " gold\n";
+        cout << "  From nobles: " << nobleTax << " gold\n";
+        cout << "  Total: " << totalTax << " gold\n";
+        peasant.updateSatisfaction(-1);
+        merchant.updateSatisfaction(-1);
+        noble.updateSatisfaction(-1);
+        break;
+    }
+    case 3: {
+        int cost = 50;
+        if (rm.hasResource("Gold", cost)) {
+            rm.consume("Gold", cost, playerId);
+            cout << "Invested " << cost << " gold in the economy.\n";
+            peasant.updateSatisfaction(5);
+            merchant.updateSatisfaction(10);
+            noble.updateSatisfaction(3);
+            rm.gather("Food", 20, playerId);
+            rm.gather("Wood", 15, playerId);
+            rm.gather("Stone", 10, playerId);
+            rm.gather("Iron", 5, playerId);
+            cout << "Economic growth has increased resources and satisfaction.\n";
+        }
+        else {
+            cout << "Not enough gold to invest in the economy.\n";
+        }
+        break;
+    }
+    case 4:
+        cout << "Returning to main menu.\n";
+        break;
+    default:
+        cout << "Invalid option.\n";
+    }
+}
+
+void Economy::display() const {
+    cout << "Economy Status:\n";
+    cout << "  Current tax rate: " << taxRate << "%\n";
+}
+
+void Economy::setTaxRate(int newRate) {
+    taxRate = newRate;
     if (taxRate < 0) taxRate = 0;
-    else if (taxRate > 50) taxRate = 50;
-    cout << "\nTax are adjusted to " << taxRate;
-}
-
-void Economy::applyInflation() {
-    inflation += 0.5f;
-    if (inflation > 10) inflation = 10;
-    cout << "\nInflation applied.\nInflation rate: " << inflation;
-}
-
-float Economy::collectTaxes(int populationSize) {
-    float collected = populationSize * (taxRate / 100.0f);
-    cout << "\nTax collected of amount: $" << collected;
-    return collected;
-}
-
-void Economy::increasePublicSpending(float amount) {
-    publicSpending += amount;
-    cout << "\nPublic spending increased by $" << amount;
-    cout << "\nTotal public spending: $" << publicSpending;
-}
-
-void Economy::printStatus() const {
-    cout << "\nTax Rate: " << taxRate << "% | Inflation: " << inflation << "%\n";
-}
-
-void Economy::saveToFile(ofstream& out) const {
-    out << taxRate << "\n";
-    out << inflation << "\n";
-    out << publicSpending << "\n";
-}
-
-void Economy::loadFromFile(ifstream& in) {
-    string line;
-    getline(in, line); taxRate = stof(line);
-    getline(in, line); inflation = stof(line);
-    getline(in, line); publicSpending = stof(line);
-}
-float Economy::getTaxRate() const {
-    return taxRate;
-}
-
-float Economy::getInflation() const {
-    return inflation;
+    if (taxRate > 50) taxRate = 50;
 }
